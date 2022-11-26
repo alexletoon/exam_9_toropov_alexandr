4d768b66983313a7254d3ebd18894c96e583c32b
Alexandr Toropov <bligs100@gmail.com>
2022-11-26 12:06:49 +0600

    Добавлена папка uploads в корень приложения и зарегистрирована в settings.py и urls.py
    создана модель Picture с ссылкой на user в качестве автора и полем m2m для добавления в избранное
    сделана миграция
    
    в папке приложения gallery_app создан файл urls.py и зарегистрирован в основном urls.py
    
    Добавлена папка static в корень проекта и зарегистрирована в settings.py

---
+++ source/gallery_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Picture(models.Model):
+    image = models.ImageField(verbose_name='Фотография', blank=False, null=False, upload_to='gallery_pics')
+    description = models.CharField(verbose_name='Подпись', blank=False, null=False, max_length=50)
+    created_at = models.DateTimeField(verbose_name='Дата загрузки', auto_now_add=True)
+    author = models.ForeignKey(verbose_name='Автор', to=User, related_name='pictures', on_delete=models.CASCADE)
+    favorites = models.ManyToManyField(to=User, related_name='favorite_pics', blank=True)
+
+
+    def __str__(self) -> str:
+        return f'author - {self.author}, description - {self.description}'
